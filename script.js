20f123a0ba692d900c68182e32d7b806d7999aa9
Thamaraikannan R <thamaraikannanr1@gmail.com>
2025-10-04 14:40:25 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    // Clear existing options
+    voiceSelect.innerHTML = '';
+
+    voices.forEach((voice, i) => {
+        let option = new Option(voice.name, voice.lang);
+        option.value = i; // Store the index as the value
+        voiceSelect.add(option);
+    });
+};
+
+document.querySelector("button").addEventListener('click', () => {
+    speech.text = document.querySelector("textarea").value;
+    speech.voice = voices[voiceSelect.value]; // Set selected voice
+    window.speechSynthesis.speak(speech);
+});
